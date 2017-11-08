04a7f3176e559aa28c44e86863fbe9fe388d9795
Luis Alfredo Nuñez <luis.alfredo.nu@gmail.com>
2017-11-08 01:00:25 -0600

    Added how get the matrix diagonalization

--- src/alignandcomparemolecule/alignandcomparemolecule.cc
+++ src/alignandcomparemolecule/alignandcomparemolecule.cc
@@ -10,6 +10,7 @@ using std::vector;
 #include "readxyzfile.h"
 #include "atomsinmolecule.h"
 #include "molecularoperations.h"
+#include "vectormatrixoperations.h"
 
 int main (int argc, char *argv[]) {
    
@@ -40,6 +41,30 @@ int main (int argc, char *argv[]) {
 			inertiatensor_molecula_A = molecularOP.inertiaTensor(molecule_A_inCM);
 			inertiatensor_molecula_B = molecularOP.inertiaTensor(molecule_B_inCM);
 
+			vector<vector<double>> initialmatrix_molecule_A (3,vector<double>(3,0.0));
+			vector<vector<double>> diagmatrix_molecule_A(3,vector<double>(3,0.0));
+			vector<vector<double>> eigvectors_molecule_A(3,vector<double>(3,0.0));
+			vector<double> eigvalues_molecule_A(3,0.0);
+
+			vector<vector<double>> initialmatrix_molecule_B (3,vector<double>(3,0.0));
+			vector<vector<double>> diagmatrix_molecule_B(3,vector<double>(3,0.0));
+			vector<vector<double>> eigvectors_molecule_B(3,vector<double>(3,0.0));
+			vector<double> eigvalues_molecule_B(3,0.0);
+
+			VectorAndMatrixOperations matrixOP;
+
+			matrixOP.eigenVectorValues(initialmatrix_molecule_A,diagmatrix_molecule_A,eigvectors_molecule_A,eigvalues_molecule_A);
+			matrixOP.eigenVectorValues(initialmatrix_molecule_B,diagmatrix_molecule_B,eigvectors_molecule_B,eigvalues_molecule_B);
+
+			cout << endl << " Diagonalization - Inertia Tensor - Molecule A" << endl;
+			for(int i=0;i<3;++i) cout << " | " << diagmatrix_molecule_A[0][i] << "\t--\t" << diagmatrix_molecule_A[1][i]<< "\t--\t" << diagmatrix_molecule_A[2][i] << " | " << endl;
+			cout << endl << " EingenValues - Inertia Tensor - Molecule A" << endl;
+			cout << " | " << eigvalues_molecule_A[0] << "\t--\t" << eigvalues_molecule_A[1]<< "\t--\t" << eigvalues_molecule_A[2] << " | " << endl;
+
+			cout << endl << " Diagonalization - Inertia Tensor - Molecule B" << endl;
+			for(int i=0;i<3;++i) cout << " | " << diagmatrix_molecule_B[0][i] << "\t--\t" << diagmatrix_molecule_B[1][i]<< "\t--\t" << diagmatrix_molecule_B[2][i] << " | " << endl;
+			cout << endl << " EingenValues - Inertia Tensor - Molecule B" << endl;
+			cout << " | " << eigvalues_molecule_B[0] << "\t--\t" << eigvalues_molecule_B[1]<< "\t--\t" << eigvalues_molecule_B[2] << " | " << endl;
 
 
 			return EXIT_SUCCESS;
