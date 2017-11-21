3cb9d11d541553c8c33b146faef13721083abee4
Luis Alfredo Nuñez <luis.alfredo.nu@gmail.com>
2017-11-21 09:39:54 -0600

    Change for rotated a molecule and get de xyz File

--- src/rotationmolecules/rotationmolecules.cc
+++ src/rotationmolecules/rotationmolecules.cc
@@ -14,33 +14,56 @@ using std::vector;
 #include "atomsinmolecule.h"
 #include "molecularoperations.h"
 #include "vectormatrixoperations.h"
+#include "output.h"
 
 int main (int argc, char *argv[]) {
    
 	ScreenUtils scrut;
+			
+	OutputAlignProgram output;
 	
-	if(argc > 2){
+	if(argc > 1){
 
-		cout << endl;
-		scrut.PrintScrStarLine();
-		scrut.SetScrGreenBoldFont();
-		cout << "Rotation molecules." << endl;
-		scrut.SetScrNormalFont();
-		scrut.PrintScrStarLine();
-		cout << endl << " Molecule A: "<< argv[1] << setw(35);
-		cout << " Molecule B: "<< argv[2] << endl;
 
 		vector<Atom> molecule_A;
-		vector<Atom> molecule_B;
 		
 		ReadXYZFile reader;
 
 		bool statusAllData_molecule_A = reader.getValuesFromFile(argv[1],molecule_A);
-		bool statusAllData_molecule_B = reader.getValuesFromFile(argv[2],molecule_B);
 
-		if(statusAllData_molecule_A && statusAllData_molecule_B){
-			
-			
+		if(statusAllData_molecule_A){
+
+			VectorAndMatrixOperations matrixOP;
+
+			double phi = 90.0;
+			double theta = 0.0;
+			double psi = 0.0;
+			phi = phi * 3.14159 / 180.0;
+			theta = theta * 3.14159 / 180.0;
+			psi = psi * 3.14159 / 180.0;
+			vector<double> angles = {phi,theta,psi};
+
+			vector<Atom> molecule_B_rotate = matrixOP.rotateMolecule(angles, molecule_A);
+
+			cout << endl << "Coordenates of molecule A"<< endl;
+			cout << molecule_A.size() << endl << endl;
+			for(unsigned int i=0;i<molecule_A.size();i++){
+				cout << molecule_A[i].atomSymbol << setw(13);
+				cout << molecule_A[i].atomCoordinates[0] <<setw(15) ;
+				cout << molecule_A[i].atomCoordinates[1] <<setw(15) ;
+				cout << molecule_A[i].atomCoordinates[2] <<setw(15) ;
+				cout << endl;
+			}
+			cout << "Coordenates of molecule B"<< endl;
+			cout << molecule_B_rotate.size() << endl;
+			cout << argv[1] << endl;
+			for(unsigned int i=0;i<molecule_B_rotate.size();i++){
+				cout << molecule_B_rotate[i].atomSymbol << setw(15);
+				cout << molecule_B_rotate[i].atomCoordinates[0] << setw(15);
+				cout << molecule_B_rotate[i].atomCoordinates[1] << setw(15);
+				cout << molecule_B_rotate[i].atomCoordinates[2];
+				cout << endl;
+			}
 			return EXIT_SUCCESS;
 		}else{
 			cout << endl;
@@ -54,7 +77,7 @@ int main (int argc, char *argv[]) {
 		cout << endl;
 		scrut.PrintScrStarLine();
 		scrut.DisplayErrorMessage("Dont input files in arguments ");
-		cout << "Example: rotationmolecules file_A.xyz file_B.xyz  "<< endl;
+		cout << "Example: alignandcomparemolecule file_A.xyz file_B.xyz  "<< endl;
 		scrut.PrintScrStarLine();
 		cout << endl;
 		return EXIT_FAILURE;
