8f3a531fdca23acaf5fc0e2a47e1c7573f1f11e7
Luis Alfredo Nuñez <luis.alfredo.nu@gmail.com>
2017-12-05 15:14:50 -0600

    Included in the main programm the method for discarting molecules with different number or type of atoms

--- src/alignandcomparemolecule/alignandcomparemolecule.cc
+++ src/alignandcomparemolecule/alignandcomparemolecule.cc
@@ -15,73 +15,83 @@ using std::vector;
 #include "output.h"
 
 int main (int argc, char *argv[]) {
-   
+
 	ScreenUtils scrut;
-			
+
 	OutputAlignProgram output;
-	
+
 	if(argc > 2){
 
 		output.displayFilesNames(argv[1],argv[2]);
 
 		vector<Atom> molecule_A;
 		vector<Atom> molecule_B;
-		
+
 		ReadXYZFile reader;
 
 		bool statusAllData_molecule_A = reader.getValuesFromFile(argv[1],molecule_A);
 		bool statusAllData_molecule_B = reader.getValuesFromFile(argv[2],molecule_B);
 
 		if(statusAllData_molecule_A && statusAllData_molecule_B){
-			
-			vector<vector<double>> inertiatensor_molecula_A(3,vector<double>(3,0));
-			vector<vector<double>> inertiatensor_molecula_B(3,vector<double>(3,0));
 
 			MolecularOperations molecularOP;
-			
-			vector<Atom> molecule_A_inCM = molecularOP.moveCM2Origin(molecule_A);
-			vector<Atom> molecule_B_inCM = molecularOP.moveCM2Origin(molecule_B);
 
-			inertiatensor_molecula_A = molecularOP.inertiaTensor(molecule_A_inCM);
-			inertiatensor_molecula_B = molecularOP.inertiaTensor(molecule_B_inCM);
-			
-			vector<vector<double>> diagmatrix_molecule_A(3,vector<double>(3,0.0));
-			vector<vector<double>> eigvectors_molecule_A(3,vector<double>(3,0.0));
-			vector<double> eigvalues_molecule_A(3,0.0);
+			if(molecularOP.haveSameTypeNumAtoms(molecule_A,molecule_B)){
 
-			vector<vector<double>> diagmatrix_molecule_B(3,vector<double>(3,0.0));
-			vector<vector<double>> eigvectors_molecule_B(3,vector<double>(3,0.0));
-			vector<double> eigvalues_molecule_B(3,0.0);
+				vector<vector<double>> inertiatensor_molecula_A(3,vector<double>(3,0));
+				vector<vector<double>> inertiatensor_molecula_B(3,vector<double>(3,0));
 
-			VectorAndMatrixOperations matrixOP;
+				vector<Atom> molecule_A_inCM = molecularOP.moveCM2Origin(molecule_A);
+				vector<Atom> molecule_B_inCM = molecularOP.moveCM2Origin(molecule_B);
 
-			matrixOP.eigenVectorValues(inertiatensor_molecula_A,diagmatrix_molecule_A,eigvectors_molecule_A,eigvalues_molecule_A);
-			matrixOP.eigenVectorValues(inertiatensor_molecula_B,diagmatrix_molecule_B,eigvectors_molecule_B,eigvalues_molecule_B);
+				inertiatensor_molecula_A = molecularOP.inertiaTensor(molecule_A_inCM);
+				inertiatensor_molecula_B = molecularOP.inertiaTensor(molecule_B_inCM);
+
+				vector<vector<double>> diagmatrix_molecule_A(3,vector<double>(3,0.0));
+				vector<vector<double>> eigvectors_molecule_A(3,vector<double>(3,0.0));
+				vector<double> eigvalues_molecule_A(3,0.0);
+
+				vector<vector<double>> diagmatrix_molecule_B(3,vector<double>(3,0.0));
+				vector<vector<double>> eigvectors_molecule_B(3,vector<double>(3,0.0));
+				vector<double> eigvalues_molecule_B(3,0.0);
+
+				VectorAndMatrixOperations matrixOP;
+
+				matrixOP.eigenVectorValues(inertiatensor_molecula_A,diagmatrix_molecule_A,eigvectors_molecule_A,eigvalues_molecule_A);
+				matrixOP.eigenVectorValues(inertiatensor_molecula_B,diagmatrix_molecule_B,eigvectors_molecule_B,eigvalues_molecule_B);
 
 
-			string title = "Inertia Tensor";
-			output.displayDualMatrix(title,inertiatensor_molecula_A,inertiatensor_molecula_B);
-		
-			title = "EingenVectors - Inertia Tensor";
-			output.displayDualMatrix(title,eigvectors_molecule_A,eigvectors_molecule_B);
-	
-			title = "EingenValues - Inertia Tensor";
-			output.displayDualMatrix(title,eigvalues_molecule_A,eigvalues_molecule_B);
-			
-			if(matrixOP.compareEigenValues(eigvalues_molecule_A,eigvalues_molecule_B)){
+				string title = "Inertia Tensor";
+				output.displayDualMatrix(title,inertiatensor_molecula_A,inertiatensor_molecula_B);
 
-				output.displayItsTheSame();
-/*
-				vector<Atom> molecule_A_align = matrixOP.rotateMolecule(eigvectors_molecule_A,molecule_A_inCM);
-				vector<Atom> molecule_B_align = matrixOP.rotateMolecule(eigvectors_molecule_B,molecule_B_inCM);
-				
-				cout << endl << "Coordenates of molecule A" << endl;
-				output.saveXYZFile(argv[1],molecule_A_align);
-				cout << "Coordenates of molecule B" << endl;
-				output.saveXYZFile(argv[2],molecule_B_align);
-*/
+				title = "EingenVectors - Inertia Tensor";
+				output.displayDualMatrix(title,eigvectors_molecule_A,eigvectors_molecule_B);
+
+				title = "EingenValues - Inertia Tensor";
+				output.displayDualMatrix(title,eigvalues_molecule_A,eigvalues_molecule_B);
+
+				if(matrixOP.compareEigenValues(eigvalues_molecule_A,eigvalues_molecule_B)){
+
+					output.displayItsTheSame();
+					/*
+						vector<Atom> molecule_A_align = matrixOP.rotateMolecule(eigvectors_molecule_A,molecule_A_inCM);
+						vector<Atom> molecule_B_align = matrixOP.rotateMolecule(eigvectors_molecule_B,molecule_B_inCM);
+
+						cout << endl << "Coordenates of molecule A" << endl;
+						output.saveXYZFile(argv[1],molecule_A_align);
+						cout << "Coordenates of molecule B" << endl;
+						output.saveXYZFile(argv[2],molecule_B_align);
+						*/
+				}
+				return EXIT_SUCCESS;
+			}else{
+				cout << endl;
+				scrut.PrintScrStarLine();
+				scrut.DisplayErrorMessage(" The Molecules do not have the same number and/or type of atoms ");
+				scrut.PrintScrStarLine();
+				cout << endl;
+				return EXIT_FAILURE;
 			}
-			return EXIT_SUCCESS;
 		}else{
 			cout << endl;
 			scrut.PrintScrStarLine();
