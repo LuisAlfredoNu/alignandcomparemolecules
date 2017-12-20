65a1f4771f4b6c02560485836bd6b67059dc68bf
Luis Alfredo Nuñez <luis.alfredo.nu@gmail.com>
2017-12-20 03:26:44 -0600

    Added a test for sorting on X coordinates

--- src/test/test_readxyzfile.cc
+++ src/test/test_readxyzfile.cc
@@ -1,4 +1,5 @@
 #include <cstdlib>
+#include <iomanip>
 #include <iostream>
 using std::cout;
 using std::endl;
@@ -24,7 +25,6 @@ int main (int argc, char *argv[])
 
 		bool statusAllData = reader.getValuesFromFile(argv[1],molecule);
 
-
 		string statusanswer;
 		for(unsigned int i=0;i<molecule.size();i++){
 			cout << "Atom = " << i << endl;
@@ -34,16 +34,36 @@ int main (int argc, char *argv[])
 			cout << "All data is fine? " << statusanswer << endl;
 			cout << "********************************************************" << endl << endl;
 		}   
-	
 		cout << "All data is fine ?  " << (statusAllData ? statusanswer="yes" : statusanswer="no") << endl;
-	
+		cout << endl << " Input Data " << endl << endl;
+		cout << "12" << endl;
+		cout << "benzene example" << endl;
+		cout << "  6        0.00000        1.40272        0.00000" << endl;
+		cout << "  1        0.00000        2.49029        0.00000" << endl;
+		cout << "  6       -1.21479        0.70136        0.00000" << endl;
+		cout << "  1       -2.15666        1.24515        0.00000" << endl;
+		cout << "  6       -1.21479       -0.70136        0.00000" << endl;
+		cout << "  1       -2.15666       -1.24515        0.00000" << endl;
+		cout << "  6        0.00000       -1.40272        0.00000" << endl;
+		cout << "  1        0.00000       -2.49029        0.00000" << endl;
+		cout << "  6        1.21479       -0.70136        0.00000" << endl;
+		cout << "  1        2.15666       -1.24515        0.00000" << endl;
+		cout << "  6        1.21479        0.70136        0.00000" << endl;
+		cout << "  1        2.15666        1.24515        0.00000" << endl;
+		cout << endl << " After read and sorting data" << endl << endl;
+		for(unsigned int i=0;i<molecule.size();i++){
+			cout << "  " << molecule[i].atomNumber;
+			for (int ii=0;ii<3;ii++) cout << std::setw(15)<< std::setprecision(6) << molecule[i].atomCoordinates[ii];
+			cout << endl;
+		}
+
+
 		return EXIT_SUCCESS;
 	}else { 
 		cout << "Dont input file in arguments " << endl << endl;
 		return EXIT_FAILURE;
-	
 	}
-
 }
+
 
 
