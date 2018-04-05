5296e190ba67dd8303617b4141febecbd3eecc9a
Luis Alfredo Nuñez <luis.alfredo.nu@gmail.com>
2018-04-05 15:39:39 -0500

    Solve the problem of stereisomer

--- src/alignandcomparemolecule/alignandcomparemolecule.cc
+++ src/alignandcomparemolecule/alignandcomparemolecule.cc
@@ -69,10 +69,18 @@ int main (int argc, char *argv[]) {
 				title = "EingenValues - Inertia Tensor";
 				output.displayDualMatrix(title,eigvalues_molecule_A,eigvalues_molecule_B);
 
+				double new_lenght = 2.0;
+				vector<vector<double>> new_eigvectors_molecule_A = matrixOP.incrementLengthVector(new_lenght,eigvectors_molecule_A);
+				vector<vector<double>> new_eigvectors_molecule_B = matrixOP.incrementLengthVector(new_lenght,eigvectors_molecule_B);
+
+				title = "New lenght of EinVec ";
+				title += std::to_string(new_lenght);
+				output.displayDualMatrix(title,new_eigvectors_molecule_A,new_eigvectors_molecule_B);
+
 				if(matrixOP.compareEigenValues(eigvalues_molecule_A,eigvalues_molecule_B)){
 
 					output.displayItsTheSame();
-				
+
 					vector<Atom> molecule_A_align = matrixOP.rotateMolecule(eigvectors_molecule_A,molecule_A_inCM);
 					vector<Atom> molecule_B_align = matrixOP.rotateMolecule(eigvectors_molecule_B,molecule_B_inCM);
 
@@ -82,18 +90,42 @@ int main (int argc, char *argv[]) {
 					if(matrixOP.compareCoordinates(molecule_A_align,molecule_B_align)){
 						cout << "The both molecules are the same 00 "<< endl;
 					}else{
-				
-						vector<vector<double>> change2_A_basis = matrixOP.changeBasisEigenVec(eigvectors_molecule_A,eigvectors_molecule_B);
-						vector<Atom> molecule_B_align_second_rotation = matrixOP.rotateMolecule(change2_A_basis,molecule_B_inCM);
+							
+						vector<double> angles (3,0.0);
+						bool is_same_after_rotation_in_Z = false;
+						vector<Atom> molecule_B_align_second_rotation;
+						
+						while(! is_same_after_rotation_in_Z){
+
+							angles[2] += 90.0;
+							molecule_B_align_second_rotation = matrixOP.rotateMolecule(angles,molecule_B_align);
+
+							if(matrixOP.compareCoordinates(molecule_A_align,molecule_B_align_second_rotation) || angles[2] == 270.0)
+								is_same_after_rotation_in_Z = true;
+						}
+							molecule_B_align = molecule_B_align_second_rotation;
+						/*
+							vector<vector<double>> change2_A_basis = matrixOP.changeBasisEigenVec(eigvectors_molecule_A,eigvectors_molecule_B);
+						vector<Atom> molecule_B_align_second_rotation = matrixOP.rotateMolecule2(change2_A_basis,molecule_B_inCM);
+
+						title = "EingenVectors - Change Basis";
+						output.displayDualMatrix(title,eigvectors_molecule_A,change2_A_basis);
 
 						inertiatensor_molecula_A = molecularOP.inertiaTensor(molecule_A_align);
 						inertiatensor_molecula_B = molecularOP.inertiaTensor(molecule_B_align_second_rotation);
 
 						matrixOP.eigenVectorValues(inertiatensor_molecula_A,diagmatrix_molecule_A,eigvectors_molecule_A,eigvalues_molecule_A);
 						matrixOP.eigenVectorValues(inertiatensor_molecula_B,diagmatrix_molecule_B,eigvectors_molecule_B,eigvalues_molecule_B);
-						
+
+						string title = "Inertia Tensor";
+						output.displayDualMatrix(title,inertiatensor_molecula_A,inertiatensor_molecula_B);
+
+						title = "EingenVectors - Inertia Tensor";
+						output.displayDualMatrix(title,eigvectors_molecule_A,eigvectors_molecule_B);
+
 						molecule_B_align = matrixOP.rotateMolecule(eigvectors_molecule_B,molecule_B_align_second_rotation);
-					
+						//molecule_B_align = molecule_B_align_second_rotation;
+*/
 						reader.sortingAtoms(molecule_A_align);
 						reader.sortingAtoms(molecule_B_align);
 
@@ -103,11 +135,33 @@ int main (int argc, char *argv[]) {
 							cout << "The both molecules are isomers " << endl;
 						}
 					}
-	
+					
+					inertiatensor_molecula_A = molecularOP.inertiaTensor(molecule_A_align);
+					inertiatensor_molecula_B = molecularOP.inertiaTensor(molecule_B_align);
+					matrixOP.eigenVectorValues(inertiatensor_molecula_A,diagmatrix_molecule_A,eigvectors_molecule_A,eigvalues_molecule_A);
+					matrixOP.eigenVectorValues(inertiatensor_molecula_B,diagmatrix_molecule_B,eigvectors_molecule_B,eigvalues_molecule_B);
+
+					string title = "Inertia Tensor";
+					output.displayDualMatrix(title,inertiatensor_molecula_A,inertiatensor_molecula_B);
+
+					title = "EingenVectors - Inertia Tensor";
+					output.displayDualMatrix(title,eigvectors_molecule_A,eigvectors_molecule_B);
+
+					title = "EingenValues - Inertia Tensor";
+					output.displayDualMatrix(title,eigvalues_molecule_A,eigvalues_molecule_B);
+
+					double new_lenght = 2.0;
+					vector<vector<double>> new_eigvectors_molecule_A = matrixOP.incrementLengthVector(new_lenght,eigvectors_molecule_A);
+					vector<vector<double>> new_eigvectors_molecule_B = matrixOP.incrementLengthVector(new_lenght,eigvectors_molecule_B);
+
+					title = "New lenght of EinVec ";
+					title += std::to_string(new_lenght);
+					output.displayDualMatrix(title,new_eigvectors_molecule_A,new_eigvectors_molecule_B);
+
 					cout << endl << "Coordenates of molecule A" << endl;
 					output.saveXYZFile(argv[1],"Molecule A",molecule_A_align);
 					output.displayXYZFile(argv[1],molecule_A_align);
-					
+
 					cout << endl << "Coordenates of molecule B" << endl;
 					output.saveXYZFile(argv[2],"MolBcule B",molecule_B_align);
 					output.displayXYZFile(argv[2],molecule_B_align);
